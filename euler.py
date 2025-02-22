8f31d3a3cd6765268528c0828c8c64e6693aa532
KHOUTAIBI <khoutaibi.iliass@gmail.com>
2025-02-22 12:19:14 +0100

    added comments

--- euler.py
+++ euler.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def euler(f,y_0,step,t_0,T):
+    """Classic Euler Method for solving differential equations"""
+    """Use this in orde to solve the problems of type y' = f(t,y)"""
+    """Choose an appropriate step size and an appropriate initial value to solve the problem"""
     
-    t_N = t_0 + T
     number_steps = np.arange(t_0, t_0 + T + step, step)
     y = [y_0]
 
@@ -21,7 +24,7 @@ def euler(f,y_0,step,t_0,T):
 def f(t,y) :
     return 2 - np.exp(-4 * t) - 2*y
 
-def exact(t):
+def exact_function(t):
     return 1 + 1/2 * np.exp(-4 * t) -1/2 * np.exp(-2 * t)
 
 number_steps , y = euler(f=f, y_0=1, step=0.05, t_0=0, T=5)
@@ -30,6 +33,6 @@ number_steps , y = euler(f=f, y_0=1, step=0.05, t_0=0, T=5)
 
 plt.grid()
 plt.plot(number_steps,y,label='Approximate Solution')
-plt.plot(number_steps,exact(number_steps),label='Approximate Solution')
+plt.plot(number_steps,exact_function(number_steps),label='Approximate Solution')
 plt.legend()
 plt.show()
